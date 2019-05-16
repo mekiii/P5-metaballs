cffb4f4d66d71eb4a95973813e72072ec4f58288
Meki <you@example.com>
2019-05-16 20:59:31 +0200

    Playing around with color

--- sketch.js
+++ sketch.js
@@ -5,17 +5,18 @@
 
 let blob;
 let yoff = 0;
-let threshold = 900;
+let threshold = 400;
 
 class Blob {
 
   constructor(x, y) {
-    this.x = x;
-    this.y = y;
+
     let angle = random(0, 2 * PI);
     this.xspeed = random(2, 5) * Math.cos(angle);
     this.yspeed = random(2, 5) * Math.sin(angle);
-    this.r = random(10,300);
+    this.r = random(100, 300);
+    this.x = random(this.r, windowWidth - this.r);
+    this.y = random(this.r, windowHeight - this.r);
   }
 
   update() {
@@ -27,24 +28,24 @@ class Blob {
 
   show() {
     noStroke();
-    noFill();
-    ellipse(this.x, this.y, this.r);
-    /*push();
+    fill(0,0.2);
+    //ellipse(this.x, this.y, this.r);
+    push();
     translate(this.x, this.y);
     beginShape();
     let xoff = 0;
-    for ( let a = 0 ; a < TWO_PI; a+= 0.1 ) {
-      let offset = map(noise(xoff,yoff), 0 , 1, -25, 25);
+    for (let a = 0; a < TWO_PI; a += 0.1) {
+      let offset = map(noise(xoff, yoff), 0, 1, -25, 25);
       let r = this.r + offset;
-      let x2 = r *cos(a);
+      let x2 = r * cos(a);
       let y2 = r * sin(a);
-      vertex(x2,y2);
-      xoff += 0.1;
+      vertex(x2, y2);
+      xoff += 0.2;
     }
     endShape();
     pop();
-    yoff += 0.01;
-    */
+    yoff += 0.2;
+
   }
 }
 
@@ -54,25 +55,26 @@ function setup() {
   colorMode(RGB, 255, 255, 255, 1);
   createCanvas(windowWidth, windowHeight);
   colorMode(HSB);
-  for (let i = 0; i < 12; i++) {
+  for (let i = 0; i < 4; i++) {
     blobs.push(new Blob(random(windowWidth), random(windowHeight)));
   };
 }
 
 
 function draw() {
-  background(0);
-  for (let w = 0; w < windowWidth; w += 20) {
-    for (let h = 0; h < windowHeight; h += 20) {
+  background(255);
+  for (let w = 0; w < windowWidth; w += 10) {
+    for (let h = 0; h < windowHeight; h += 10) {
       let sum = 0;
       for (let j = 0; j < blobs.length; j++) {
         let v1 = createVector(blobs[j].x, blobs[j].y)
         let v2 = createVector(w, h);
         let d = v1.dist(v2);
-        sum += 150 * blobs[j].r/d;
+        sum += 150 * blobs[j].r / d;
         if (sum > threshold) {
-          fill(sum, 0.4)
-          ellipse(w, h, 10,10)
+          let color = map(sum, 0, sum, sum, 0);
+          fill(color, 0.4);
+          ellipse(w, h, 3, 3);
         }
       }
     }
